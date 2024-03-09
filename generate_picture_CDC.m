08005ce4e69813d0bc4a858bf46682eb2deba87c
Licio Romao <licio.romao@gmail.com>
2024-03-09 13:52:09 -0800

    CDC code

---
+++ generate_picture_CDC.m
@@ -0,0 +1,38 @@
+% Script to generate plot for the CDC paper
+
+load('CDC_2.mat')
+close all
+figure 
+hold on
+box on
+grid on
+load(results{1}.paths_results(2).full_path)
+plot(state_partition.get_values.partition.grid_x,value_func_kernel_KME.value_function(:,1),'--b','LineWidth',1.5) % radius equal to zero
+load(results{1}.paths_results(3).full_path)
+plot(state_partition.get_values.partition.grid_x,value_func_kernel_KME.value_function(:,1),'--*b','LineWidth',1.5) % radius equal to 0.01
+load(results{1}.paths_results(4).full_path)
+plot(state_partition.get_values.partition.grid_x,value_func_kernel_KME.value_function(:,1),'-.b','LineWidth',1.5) % radius equal to 0.05
+load(results{1}.paths_results(5).full_path)
+plot(state_partition.get_values.partition.grid_x,value_func_moment.value_function(:,1),'r','LineWidth',2) % radius mean = 0.2, radius variance = 1.5
+
+ll = legend({'Kernel ambiguity $(\\\epsilon = 0)$','Kernel ambiguity ($\\\epsilon = 0.01$)',...
+                'Kernel ambiguity ($\\\epsilon$ = 0.05)','Moment ambiguity ($b = 0.1, c = 1.5$, see [23])'},...
+                    'Interpreter','latex','FontSize',14,'Orientation','vertical','Position',[0.58,0.20,0.1,0.1]);
+set(ll,'color','none');
+
+title_text = {'Comparison of safe probability'};
+
+title(title_text,'Interpreter','latex','FontSize',18)
+
+title_x = {'Initial temperature'};
+xlabel(title_x,'Interpreter','latex','FontSize',18)
+
+title_y = {'Safety probability'};
+ylabel(title_y,'Interpreter','latex','FontSize',18)
+
+
+path_to = '/Users/licioromao/Library/CloudStorage/GoogleDrive-licio.romao@gmail.com/PhD-postdoc/Postdoc/Oxford-CS/Papers/Ashish_collaboration/CDC-paper/Extended-CDC-version/';
+
+kernel_versus_moment = gcf;
+temp = strcat(path_to,'kernel-versus-moment-new.pdf');
+exportgraphics(kernel_versus_moment,temp)
